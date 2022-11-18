2c5212199e6b04eafa3d7db5b33d86de8f8d4d62
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-11-18 01:12:27 -0500

    created a config file

---
+++ src/assets/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  apiEndpoint: 'https://gutendex.com/books/',
+  sortEndpoint: 'https://gutendex.com/books/?sort=',
+  searchEndpoint: 'https://gutendex.com/books/?search=',
+}
