237503809f2310313c207156c6526cc52fa45a04
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2022-11-18 01:14:20 -0500

    added table body with populated data

--- src/components/Books.js
+++ src/components/Books.js
@@ -15,6 +15,16 @@ const Books = (props) => {
           ))}
         </tr>
       </thead>
+      <tbody>
+        {props.books.map(book => (
+          <tr key={book.id}>
+            <td id={config.columns[0].accessor}>{book.id}</td>
+            <td id={config.columns[1].accessor}>{book.title}</td>
+            <td id={config.columns[2].accessor}>{book.download_count}</td>
+            <td id={config.columns[3].accessor}>{book.authors.length > 0 ? book.authors[0].name : 'N/A'}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
